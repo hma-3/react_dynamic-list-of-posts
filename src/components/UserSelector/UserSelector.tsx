b4d934ba5ea3c500f26773e8a7a457ddf055f4c3
Mariia <maria.gula0307@gmail.com>
2024-10-22 18:24:42 +0300

    solution

---
+++ src/components/UserSelector/UserSelector.tsx
@@ -0,0 +1,79 @@
+import { FC, useEffect, useState } from 'react';
+import cn from 'classnames';
+
+import { User } from '../../types';
+import { getUsers } from '../../services';
+
+interface Props {
+  selectedUser: User | null;
+  onSelectedUser: (user: User | null) => void;
+  onLoadPosts: (userId: User['id']) => void;
+}
+
+export const UserSelector: FC<Props> = ({
+  selectedUser,
+  onSelectedUser,
+  onLoadPosts,
+}) => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [isActiveDropdown, setIsActiveDropdown] = useState(false);
+
+  const handleSelectUser = (user: User) => {
+    onSelectedUser(user);
+    setIsActiveDropdown(false);
+    onLoadPosts(user.id);
+  };
+
+  useEffect(() => {
+    getUsers()
+      .then(setUsers)
+      .catch(() => {});
+  }, []);
+
+  return (
+    <div
+      data-cy="UserSelector"
+      className={cn('dropdown', {
+        'is-active': isActiveDropdown,
+      })}
+    >
+      <div className="dropdown-trigger">
+        <button
+          type="button"
+          className="button"
+          aria-haspopup="true"
+          aria-controls="dropdown-menu"
+          onClick={() => setIsActiveDropdown(current => !current)}
+          onBlur={() => setIsActiveDropdown(false)}
+        >
+          <span>{selectedUser ? selectedUser.name : 'Choose a user'}</span>
+
+          <span className="icon is-small">
+            <i className="fas fa-angle-down" aria-hidden="true" />
+          </span>
+        </button>
+      </div>
+
+      <div className="dropdown-menu" id="dropdown-menu" role="menu">
+        <div className="dropdown-content">
+          {users.map(user => {
+            const { id, name } = user;
+
+            return (
+              <a
+                key={id}
+                href={`#user-${id}`}
+                className={cn('dropdown-item', {
+                  'is-active': selectedUser?.id === id,
+                })}
+                onMouseDown={() => handleSelectUser(user)}
+              >
+                {name}
+              </a>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
